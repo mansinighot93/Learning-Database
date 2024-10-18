151862fa472bae934af37fe43915dddd0bb226ff
mansinighot93 <mansinighot93@gmail.com>
2024-10-18 13:04:53 +0530

    changes

--- DataBase/Events.sql
+++ DataBase/Events.sql
@@ -27,3 +27,43 @@ DROP PROCEDURE my_procedure;
 
 -- 5.Remove the event from event scheduler
 	DROP EVENT IF EXISTS my_event;
+
+
+CREATE DATABASE IF NOT EXISTS mydb;
+
+USE mydb;
+
+SELECT * FROM messages;
+SHOW EVENTS FROM mydb;
+
+CREATE TABLE IF NOT EXISTS messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    message VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT NOW()
+);
+
+CREATE EVENT IF NOT EXISTS one_time_log
+ON SCHEDULE AT CURRENT_TIMESTAMP
+DO
+  INSERT INTO messages(message)
+  VALUES('One-time event');
+  
+CREATE EVENT one_minute_log
+ON SCHEDULE AT CURRENT_TIMESTAMP + INTERVAL 1 MINUTE
+ON COMPLETION PRESERVE
+DO
+   INSERT INTO messages(message)
+   VALUES('Preserved One-minute event');
+
+CREATE EVENT recurring_log
+ON SCHEDULE EVERY 1 MINUTE
+STARTS CURRENT_TIMESTAMP
+ENDS CURRENT_TIMESTAMP + INTERVAL 1 HOUR
+DO
+   INSERT INTO messages(message)
+   VALUES(CONCAT('Running at ', NOW()));
+
+  
+
+  
+  
